5e13c9d8ee52824146dbbe59926657a102ce1c49
Alief Azies <Alief.ahmadazies@gmail.com>
2020-05-15 17:15:15 +0700

    menambahkan file untuk menyimpan feed dari xml

--- app/src/main/java/com/ziesapp/top10downloader/MainActivity.kt
+++ app/src/main/java/com/ziesapp/top10downloader/MainActivity.kt
@@ -4,11 +4,23 @@ import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import java.io.IOException
-import java.lang.Exception
-import java.net.HttpURLConnection
-import java.net.MalformedURLException
 import java.net.URL
+
+class FeedEntry{
+    var name: String = ""
+    var artist: String = ""
+    var releaseDate:String = ""
+    var summary:String = ""
+    var imageURL:String = ""
+    override fun toString(): String {
+        return """
+            name = $name
+            artist = $artist
+            releaseDate = $releaseDate
+            imageURL = $imageURL
+        """.trimIndent()
+    }
+}
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
@@ -27,9 +39,11 @@ class MainActivity : AppCompatActivity() {
         private class DownloadData : AsyncTask<String, Void, String>() {
             private val TAG = "DownloadData"
 
-            override fun onPostExecute(result: String?) {
+            override fun onPostExecute(result: String) {
                 super.onPostExecute(result)
-                Log.d(TAG, "onPostExecute: parameter is $result")
+//                Log.d(TAG, "onPostExecute: parameter is $result")
+                val parseApplications = ParseApplications()
+                parseApplications.parse(result)
             }
 
             override fun doInBackground(vararg url: String?): String {
@@ -42,53 +56,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             private fun downloadXML(urlPath:String?):String{
-                val xmlResult = StringBuilder()
-
-                try {
-                    val url = URL(urlPath)
-                    val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
-                    val response = connection.responseCode
-                    Log.d(TAG, "downloadXML: The response code was $response")
-
-//                    val reader = BufferedReader(InputStreamReader(connection.inputStream))
-//
-//                    val inputBuffer = CharArray(500)
-//                    var charsRead = 0
-//                    while (charsRead >= 0) {
-//                        charsRead = reader.read(inputBuffer)
-//                        if (charsRead > 0) {
-//                            xmlResult.append(String(inputBuffer, 0, charsRead))
-//                        }
-//                    }
-//                    reader.close()
-
-//                    val stream = connection.inputStream
-                    connection.inputStream.buffered().reader().use{xmlResult.append(it.readText())}
-                    Log.d(TAG, "Received ${xmlResult.length} bytes")
-                    return xmlResult.toString()
-
-//                } catch (e: MalformedURLException) {
-//                    Log.e(TAG, "downloadXML: Invalid URL ${e.message}")
-//                } catch (e: IOException) {
-//                    Log.e(TAG, "downloadXML: IO Exception reading data: ${e.message}")
-//                }catch(e:SecurityException){
-//                    e.printStackTrace()
-//                    Log.e(TAG, "downloadXML: Security exception. Needs Permission? ${e.message}")
-//                } catch (e: Exception) {
-//                    Log.e(TAG, "Unknown error: ${e.message}")
-//                }
-
-                }catch (e:Exception){
-                   val errorMessage:String = when(e){
-                        is MalformedURLException -> "downloadXML : Invalid URL ${e.message}"
-                        is IOException -> "download XML : IO Exception reading data: ${e.message}"
-                        is SecurityException -> {e.printStackTrace()
-                            "download XML: Security Exception. Needs Permission? ${e.message}"
-                        }
-                        else -> "Unknown Error: ${e.message}"
-                    }
-                }
-                return ""  // If it gets to here, there's been a problem. Return an empty string
+                return URL(urlPath).readText()
             }
         }
     }
